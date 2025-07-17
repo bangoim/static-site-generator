c7acc46bfad4d1c977e04c437ca3a7dc7c74e9d1
bangoim <joaocbbc@outlook.com>
2025-07-17 19:47:24 -0300

    update gitignore and also create a copystatic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
-def main(text, text_type, url):
-    node = TextNode(text, TextType.LINK, url)
-    print(node)
+from copystatic import copy_files_recursive
 
-node = main('This is some anchor text', 'link', 'https://www.boot.dev')
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+
+main()
